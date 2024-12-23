05efbcfbe1917ac5478987b7a7cb5c6b5302d104
minjeongHEO <hmjoung9770@gmail.com>
2024-12-23 16:47:18 +0900

    feat: 첫페이지 생성 #1

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,20 @@
+import Image from 'next/image';
+import logo from '@/public/images/cracker.png';
+import styles from './page.module.scss';
+
 export default function Home() {
-  return <div>dddddd</div>;
+  return (
+    <div className={styles.main_container}>
+      <div className={styles.logo__image_container}>
+        <div className={styles.shadow_box} />
+        <Image className={styles.logo__image} src={logo} alt="Job Cracker Logo" width={208} height={208} />
+      </div>
+
+      <h1 className={styles.title}>Job-Cracker</h1>
+      <div className={styles.description}>취뽀 팔로♾️로미😉</div>
+      <div className={styles.sub_title}>AI와 함께하는 맞춤형 면접 준비</div>
+
+      <button className={styles.button}>시작하기</button>
+    </div>
+  );
 }
