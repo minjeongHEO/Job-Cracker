2f7e61f401802f95be8207b6d1ab39229b594f48
minjeongHEO <hmjoung9770@gmail.com>
2024-12-23 15:35:32 +0900

    chore: nextConfig 에 sassOption추가 #4

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+import path from 'path';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'app', '_styles')],
+    prependData: `@use "./_variables.scss" as *;`,
+  },
 };
 
 export default nextConfig;
