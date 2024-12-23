66ad847ece6ffe69237401e3a44fe429da24d8a2
minjeongHEO <hmjoung9770@gmail.com>
2024-12-23 22:32:37 +0900

    feat: 개발자 선택 페이지 레이아웃 작업 #7

--- app/interview/developer/page.tsx
+++ app/interview/developer/page.tsx
@@ -1,3 +1,20 @@
+import SelectButton from '@/app/_components/SelectButton';
+
+import styles from './DeveloperPage.module.scss';
+
 export default function DeveloperPage() {
-  return <div>당신은 어떤 개발자인가요?</div>;
+  return (
+    <div className={styles.main}>
+      <div className={styles['main__description']}>
+        <div className={styles['main__title']}>당신은 어떤 개발자인가요?</div>
+        <div className={styles['main__sub-title']}>관심 있는 개발 분야를 선택해주세요.</div>
+      </div>
+
+      <div className={styles['main__selectBox']}>
+        <SelectButton />
+        <SelectButton />
+        <SelectButton />
+      </div>
+    </div>
+  );
 }
