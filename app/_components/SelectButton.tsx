2e57ab8fed1dfc544317109b247539cb647a6f20
minjeongHEO <hmjoung9770@gmail.com>
2024-12-26 15:16:30 +0900

    feat: 공통 선택버튼 컴포넌트 생성 #6

---
+++ app/_components/SelectButton.tsx
@@ -0,0 +1,42 @@
+import clsx from 'clsx';
+import { FC, SVGProps } from 'react';
+
+import styles from './SelectButton.module.scss';
+
+interface SelectButtonProps {
+  variant: 'simple' | 'detailed'; // 버튼 스타일 구분
+  title: string;
+  description?: string;
+  subTitles?: string[];
+  Icon?: FC<SVGProps<SVGSVGElement>>; // SVG 컴포넌트 타입
+  isSelected?: boolean;
+  onClick?: () => void;
+}
+
+export default function SelectButton({
+  variant = 'simple',
+  title,
+  description,
+  subTitles,
+  Icon,
+  isSelected = false,
+  onclick,
+}: SelectButtonProps) {
+  return (
+    <div className={clsx(styles.wrapper, { [styles['wrapper--selected']]: isSelected })}>
+      <div className={styles['wrapper__title']}>
+        {/* {variant === 'detailed' && Icon && <Icon />} //TODO: SVGR설정하여 아이콘 처리*/}
+        <div>{title}</div>
+      </div>
+
+      {variant === 'detailed' && (
+        <>
+          <div className={styles['wrapper__description']}>{description}</div>
+          <div className={styles['wrapper__sub-title']}>
+            {subTitles && subTitles.length > 0 && subTitles?.map((e, idx) => <span key={`subTitle-${idx}`}>{e}</span>)}
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
