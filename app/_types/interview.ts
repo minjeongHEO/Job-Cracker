1bd1a7497f5737b14ef5102a3f9b901fd2d6db17
minjeongHEO <hmjoung9770@gmail.com>
2024-12-26 22:39:33 +0900

    refactor: 타입 중복 처리 #7

--- app/_types/interview.ts
+++ app/_types/interview.ts
@@ -2,21 +2,11 @@ import { FC, SVGProps } from 'react';
 
 export type DeveloperType = 'FrontEnd' | 'BackEnd' | 'iOS';
 
-export type TopicDetails = string[];
-
-export type TechTopics = {
-  [key: string]: TopicDetails;
-};
-
-export type TopicsType = {
-  [K in DeveloperType]: TechTopics;
-};
-
 export type OptionDetails = {
   Icon: FC<SVGProps<SVGSVGElement>>; // SVG 컴포넌트 타입
   title: string;
   description: string;
-  subTitles: string[];
+  topics: { [key: string]: string[] };
 };
 
 export type DeveloperOptionType = {
