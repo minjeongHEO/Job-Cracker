e1531a4c5d05cc90fb68a3cf021803726a62bd78
minjeongHEO <hmjoung9770@gmail.com>
2024-12-27 17:45:25 +0900

    feat: 선택한 토픽의 유효성 체크 #8

--- app/interview/developer/[devType]/prepare/page.tsx
+++ app/interview/developer/[devType]/prepare/page.tsx
@@ -2,6 +2,7 @@
 import { usePathname, useSearchParams } from 'next/navigation';
 
 import TopicSelector from '@/app/_components/TopicSelector';
+import { DeveloperType } from '@/app/_types/interview';
 import { isDeveloperType } from '@/app/_utils/typeGuards';
 import { DEVELOPER_OPTIONS } from '@/app/interview/_constants/developers';
 import styles from '@/app/interview/developer/DeveloperPage.module.scss';
@@ -10,15 +11,21 @@ export default function PreparePage() {
   const pathname = usePathname(); //  `/interview/developer/FrontEnd/prepare`
   const searchParams = useSearchParams(); // ReadonlyURLSearchParams
 
+  const isTopicByDevType = (devType: DeveloperType, topicsParam: string) => {
+    if (topicsParam === 'all') return true;
+    const validTopics = Object.keys(DEVELOPER_OPTIONS[devType].topics);
+    return topicsParam.split(',').every((topic) => validTopics.includes(topic));
+  };
+
   const devType = pathname.split('/')[3];
   if (!devType || !isDeveloperType(devType)) {
     return <div>Invalid developer type</div>;
   }
 
-  const topicsParam = searchParams.get('topics'); // TODO: 타입 체크
-  // if (!topicParam || !isTopicType(devType, topicsParam)) {
-  //   return <div>Invalid topic type</div>;
-  // }
+  const topicsParam = searchParams.get('topics');
+  if (!topicsParam || !isTopicByDevType(devType, topicsParam)) {
+    return <div>Invalid topic type</div>;
+  }
 
   const selectedTopics =
     topicsParam === 'all' ? Object.keys(DEVELOPER_OPTIONS[devType].topics) : topicsParam.split(',');
