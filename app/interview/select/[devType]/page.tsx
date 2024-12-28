4373286ae0da2519ad744b75058ed1f3c0604418
minjeongHEO <hmjoung9770@gmail.com>
2024-12-28 20:40:46 +0900

    refactor: 개발자 선택 후 유효성 체크 수정 #8
    - 개발자 타입이 잘못된 경우
    - 선택한 개발자 타입에 해당하는 토픽이 없을 경우

--- app/interview/select/[devType]/page.tsx
+++ app/interview/select/[devType]/page.tsx
@@ -1,14 +1,26 @@
 import TopicSelector from '@/app/_components/TopicSelector';
 import { DeveloperType } from '@/app/_types/interview';
+import { isDeveloperType } from '@/app/_utils/typeGuards';
 import { DEVELOPER_OPTIONS } from '@/app/interview/_constants/developers';
-
-import SelectLayout from '../layout';
+import SelectLayout from '@/app/interview/select/layout';
 
 export default async function DevTypePage({ params: { devType } }: { params: { devType: DeveloperType } }) {
+  if (!devType || !isDeveloperType(devType)) {
+    return (
+      <SelectLayout title={'개발자 타입 오류'}>
+        <div>잘못된 개발자 타입입니다.</div>
+      </SelectLayout>
+    );
+  }
+
   const selectedDevOptions = Object.keys(DEVELOPER_OPTIONS[devType].topics);
 
-  if (!selectedDevOptions) {
-    return <div>Invalid developer type</div>;
+  if (!selectedDevOptions?.length) {
+    return (
+      <SelectLayout title={'주제 로드 오류'}>
+        <div>{devType} 개발자 유형에 대한 주제를 찾을 수 없습니다.</div>
+      </SelectLayout>
+    );
   }
 
   return (
