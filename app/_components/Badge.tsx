33a6bb6b2609c9874afbcb49b5b1a1ccc56791f4
Woody <96780693+minjeongHEO@users.noreply.github.com>
2025-01-03 18:59:46 +0900

    공통 질문카드 컴포넌트 구현 및 Sass 구조화 (#25)
    * refactor: Sass 단순 변수들에서 구조화된 맵으로 변경 및 적용 #20
    
    * feat: 중요도 표시하는 뱃지 컴포넌트 생성 #20
    
    * style: 뱃지 텍스트 가운데 정렬 #20
    
    * style: 변수 선언 후 css함수에 사용 #20
    
    - SCSS의 color() 함수는 CSS 내장 함수인 linear-gradient() 내부에서 직접 사용할 수 없다
    
    * style: variable색상 추가 #20
    
    * feat: 스퀘어 타입 버튼 스타일 추가 #20
    
    * feat: 박스타입 mixin 생성 및 적용 #20
    
    * feat: 질문 카드 컴포넌트 생성 #20
    
    * style: 뱃지 패딩, 폰트사이즈 변경 #20
    
    * refactor: IMPORTANCE_LEVEL 타입 변경 및 해체 할당 #20
    
    * style: chat page 스타일 적용 #3
    
    * style: interviewChat 스타일 적용 #3

---
+++ app/_components/Badge.tsx
@@ -0,0 +1,11 @@
+import clsx from 'clsx';
+
+import styles from './Badge.module.scss';
+
+export default function Badge({
+  option: { title, shade = '05' },
+}: {
+  option: { title: string; shade?: '01' | '02' | '03' | '04' | '05' };
+}) {
+  return <div className={clsx(styles.wrapper, shade && styles[`wrapper-${shade}`])}>{title}</div>;
+}
--- app/_components/InterviewChat.tsx
+++ app/_components/InterviewChat.tsx
@@ -1,4 +1,8 @@
 'use client';
+import QuestionCard from '@/app/_components/QuestionCard';
+
+import styles from './InterviewChat.module.scss';
+
 interface InterviewChatProps {
   devType: string;
   topics: string[];
@@ -6,10 +10,14 @@ interface InterviewChatProps {
 }
 export default function InterviewChat({ devType, topics, subTopics }: InterviewChatProps) {
   return (
-    <div>
+    <div className={styles.main}>
       <div>{devType}</div>
       <div>{topics.join(',')}</div>
       <div>{subTopics.join(',')}</div>
+      질문답변 페이지
+      <QuestionCard isSelected={false} level={'01'} />
+      <QuestionCard isSelected={false} level={'02'} />
+      <QuestionCard isSelected={false} level={'03'} />
     </div>
   );
 }
