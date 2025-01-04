150b18feff938e8a77bed1f32d84401dc567a342
minjeongHEO <hmjoung9770@gmail.com>
2025-01-04 15:57:11 +0900

    refactor: 뱃지 쉐이드 타입 분리 #21

--- app/_components/atoms/Badge.tsx
+++ app/_components/atoms/Badge.tsx
@@ -1,11 +1,13 @@
 import clsx from 'clsx';
 
+import { BadgeShadeType } from '@/app/_types/interview';
+
 import styles from './Badge.module.scss';
 
 export default function Badge({
   option: { title, shade = '05' },
 }: {
-  option: { title: string; shade?: '01' | '02' | '03' | '04' | '05' };
+  option: { title: string; shade?: BadgeShadeType };
 }) {
   return <div className={clsx(styles.wrapper, shade && styles[`wrapper-${shade}`])}>{title}</div>;
 }
