9eacda4cdd1b90dcb18cb8218135de1261a59e61
minjeongHEO <hmjoung9770@gmail.com>
2025-01-04 15:58:57 +0900

    refactor: 질문카드의 버튼보기를 눌렀을 때 클릭핸들러 실행 및 질문 props로 받기 #21

--- app/_components/molecules/QuestionCard.tsx
+++ app/_components/molecules/QuestionCard.tsx
@@ -2,19 +2,20 @@
 
 import clsx from 'clsx';
 
-import Badge from '../atoms/Badge';
-import CardStyles from './QuestionCard.module.scss';
+import Badge from '@/app/_components/atoms/Badge';
+import { BadgeShadeType } from '@/app/_types/interview';
 
-type ShadeType = '01' | '02' | '03' | '04' | '05';
+import CardStyles from './QuestionCard.module.scss';
 
 interface QuestionCardProps {
   // question: string;
   isSelected?: boolean;
-  onClick?: () => void;
-  level?: ShadeType;
+  onClick: (question: string) => void;
+  level?: BadgeShadeType;
+  question: string;
 }
 
-export const IMPORTANCE_LEVEL: Record<ShadeType, { title: string; shade: ShadeType }> = {
+export const IMPORTANCE_LEVEL: Record<BadgeShadeType, { title: string; shade: BadgeShadeType }> = {
   '01': { title: '최우선 🚨', shade: '01' },
   '02': { title: '필수 ⭐️⭐️⭐️', shade: '02' },
   '03': { title: '중요 ⭐️⭐️', shade: '03' },
@@ -22,23 +23,29 @@ export const IMPORTANCE_LEVEL: Record<ShadeType, { title: string; shade: ShadeTy
   '05': { title: '심화', shade: '05' },
 };
 
-export default function QuestionCard({ isSelected = false, onClick, level = '05' }: QuestionCardProps) {
+export default function QuestionCard({
+  isSelected = false,
+  onClick = () => {},
+  level = '05',
+  question,
+}: QuestionCardProps) {
   const { title, shade } = IMPORTANCE_LEVEL[level];
-
   return (
-    <div className={wrapperClass(isSelected)} onClick={onClick} aria-label={'질문'}>
+    <div className={wrapperClass(isSelected)} aria-label={'질문'}>
       <header className={CardStyles['question-card__header']}>
         <span className={CardStyles['question-card__topic']}>주제</span>
         <Badge option={{ title, shade }}></Badge>
       </header>
 
       <section className={CardStyles['question-card__question']}>
-        <p className={CardStyles['question-card__question-content']}>Q. React의 장점은 무엇인가요?</p>
+        <p className={CardStyles['question-card__question-content']}>Q. {question}</p>
       </section>
 
       <footer className={CardStyles['question-card__answer']}>
         <span className={CardStyles['question-card__score']}>점수</span>
-        <button className={CardStyles['question-card__button--view']}>답변 보기</button>
+        <button className={CardStyles['question-card__button--view']} onClick={() => onClick(question)}>
+          답변 보기
+        </button>
         <button className={CardStyles['question-card__button--new']}>다른 주제</button>
       </footer>
     </div>
