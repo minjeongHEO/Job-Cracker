cf4d5c6311ad96c5d40ab6f36a4b998eb353eb13
minjeongHEO <hmjoung9770@gmail.com>
2025-01-05 05:28:23 +0900

    refactor: 주제 잘못나오는 부분 시스템 프롬프트 수정 #22

--- app/_lib/interview.ts
+++ app/_lib/interview.ts
@@ -4,6 +4,8 @@ import { GenerateAnotherQuestionRequest, GenerateQuestionRequest } from '@/app/_
 import openai from './openai';
 
 const getSystemPrompt = ({ devType, topics, subTopics }: GenerateQuestionRequest) => {
+  const topic = topics.join(', ');
+  const subTopic = subTopics.join(', ');
   const systemPrompt = `당신은 기술 면접을 통해서 ${devType} 개발자를 뽑는 개발자와 면접관 경력이 10년 이상의 Tech Lead입니다.
 다음 형식의 JSON으로 응답해주세요:
 {
@@ -20,7 +22,7 @@ const getSystemPrompt = ({ devType, topics, subTopics }: GenerateQuestionRequest
 - 04(⭐⭐): 기본적이고 가벼운 지식
 - 05(⭐): 알면 좋은 가산점 지식
 
-주어진 topic(${topics.join(', ')})과 subTopic(${subTopics.join(', ')})에 대해 
+주어진 topic(${topic})과 subTopic(${subTopic})에 대해 
 실제 면접에서 자주 나오는 중요한 질문을 생성해주세요.
 
 응답시 주의사항:
@@ -28,7 +30,7 @@ const getSystemPrompt = ({ devType, topics, subTopics }: GenerateQuestionRequest
 2. 키워드는 3-5개 정도가 적당합니다.
 3. 키워드는 질문에 대한 답을 연상할 수 있도록 도와줄 수 있는 핵심 키워드거나, 추가 학습을 위한 관련 검색 키워드를 제안해주세요.
 4. 난이도는 주어진 기준에 맞게 정확히 판단해주세요.
-5. titleTopic은 생성한 질문이 주어진 topics 중 어느 주제에 해당하는지 명시해주세요.`;
+5. titleTopic은 생성한 질문이 주어진 topic(${topic})중 어느 주제에 해당하는지 명시해주세요.`;
 
   return systemPrompt;
 };
@@ -36,8 +38,8 @@ const getSystemPrompt = ({ devType, topics, subTopics }: GenerateQuestionRequest
 /** 초기 질문 생성 */
 export async function generateQuestion({ devType, subTopics, topics }: GenerateQuestionRequest) {
   const systemPrompt = getSystemPrompt({ devType, topics, subTopics });
-  const userPrompt = `${topics.join(',')}의 ${subTopics.join(
-    ','
+  const userPrompt = `${topics.join(', ')}의 ${subTopics.join(
+    ', '
   )}에 관련한 면접 질문을 중요도가 높은걸 우선 순위로 하여 랜덤으로 생성해주세요.`;
 
   try {
