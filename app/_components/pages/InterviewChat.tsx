7c2eea7c87aa6b2ceff1b5204a999fa605074724
minjeongHEO <hmjoung9770@gmail.com>
2025-01-04 16:00:55 +0900

    feat: 인터뷰 페이지의 클릭한 질문 상태관리 및 트랜지션 적용 #21

--- app/_components/pages/InterviewChat.tsx
+++ app/_components/pages/InterviewChat.tsx
@@ -1,23 +1,55 @@
 'use client';
 
+import clsx from 'clsx';
+import { useState } from 'react';
+
+import { DeveloperType } from '@/app/_types/interview';
+
 import AnswerSection from './AnswerSection';
 import styles from './InterviewChat.module.scss';
 import QuestionSection from './QuestionSection';
 
 interface InterviewChatProps {
-  devType: string;
+  devType: DeveloperType;
   topics: string[];
   subTopics: string[];
 }
 export default function InterviewChat({ devType, topics, subTopics }: InterviewChatProps) {
+  const [clickedQuestion, setClickedQuestion] = useState<string | null>(null);
+
+  const handleQuestionClick = (question: string) => {
+    setClickedQuestion(question);
+  };
+
   return (
-    <div className={styles.main}>
-      <div>{devType}</div>
+    <div className={clsx(styles['interview-chat'], { [styles['interview-chat--with-answer']]: clickedQuestion })}>
+      {/* <div>{devType}</div>
       <div>{topics.join(',')}</div>
       <div>{subTopics.join(',')}</div>
-      질문답변 페이지
-      <QuestionSection />
-      <AnswerSection />
+      질문답변 페이지 */}
+      <QuestionSection onClick={handleQuestionClick} clickedQuestion={clickedQuestion} />
+      <AnswerSection
+        clickedQuestion={clickedQuestion}
+        level={{ title: '최우선 🚨', shade: '01' }}
+        keywords={[
+          '키워드 1',
+          '키워드 2',
+          '키워드 3',
+          '키워드 4',
+          '키워드 5',
+          '키워드 6',
+          '키워드 7',
+          '키워드 8',
+          '키워드 9',
+          '키워드 10',
+        ]}
+        score={100}
+        answer={'내답변은 이거야'}
+        feedback={
+          '피드백 내용은 이렇습니다. 피드백 내용은 이렇습니다. 피드백 내용은 이렇습니다. 피드백 내용은 이렇습니다. 피드백 내용은 이렇습니다. 피드백 내용은 이렇습니다. 피드백 내용은 이렇습니다. 피드백 내용은 이렇습니다. 피드백 내용은 이렇습니다. 피드백 내용은 이렇습니다. 피드백 내용은 이렇습니다. 피드백 내용은 이렇습니다. 피드백 내용은 이렇습니다. 피드백 내용은 이렇습니다. '
+        }
+        improvedAnswer={'100점짜리 답변은 이거다'}
+      />
     </div>
   );
 }
