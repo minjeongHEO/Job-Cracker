5ca0cf0b4aee416db5451aa1be250224ce5c83a7
minjeongHEO <hmjoung9770@gmail.com>
2025-01-04 15:59:57 +0900

    feat: 목데이터로 질문카드들 만들고 사이드바가 나왔을 때 트렌지션 적용 #21

--- app/_components/pages/QuestionSection.tsx
+++ app/_components/pages/QuestionSection.tsx
@@ -1,13 +1,44 @@
 'use client';
 
-import QuestionCard from '../molecules/QuestionCard';
+import clsx from 'clsx';
 
-export default function QuestionSection() {
+import QuestionCard from '@/app/_components/molecules/QuestionCard';
+
+import styles from './QuestionSection.module.scss';
+
+const mock_questions = [
+  'React의 장점은 무엇인가요?',
+  'Next.js의 장점은 무엇인가요?',
+  'TypeScript의 장점은 무엇인가요?',
+] as string[];
+
+interface QuestionSectionProps {
+  onClick: (question: string) => void;
+  clickedQuestion: string | null;
+}
+
+export default function QuestionSection({ onClick, clickedQuestion }: QuestionSectionProps) {
   return (
-    <div>
-      <QuestionCard isSelected={false} level={'01'} />
-      <QuestionCard isSelected={false} level={'02'} />
-      <QuestionCard isSelected={false} level={'03'} />
+    <div className={clsx(styles['question_section'], { [styles['question_section--with-answer']]: clickedQuestion })}>
+      <header>질문 섹션</header>
+      <QuestionCard
+        question={mock_questions[0]}
+        isSelected={mock_questions[0] === clickedQuestion}
+        onClick={onClick}
+        level={'01'}
+      />
+      <QuestionCard
+        question={mock_questions[1]}
+        isSelected={mock_questions[1] === clickedQuestion}
+        onClick={onClick}
+        level={'02'}
+      />
+      <QuestionCard
+        question={mock_questions[2]}
+        isSelected={mock_questions[2] === clickedQuestion}
+        onClick={onClick}
+        level={'03'}
+      />
     </div>
   );
 }
