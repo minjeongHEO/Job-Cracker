f6c22ff061613737ae5049894959f5a585687a3f
minjeongHEO <hmjoung9770@gmail.com>
2025-01-12 19:08:35 +0900

    refactor: 뱃지 컴포넌트 props type을 인터페이스로 분리 및 동적클래스 함수로 분리 #33

--- app/_components/atoms/Badge.tsx
+++ app/_components/atoms/Badge.tsx
@@ -4,10 +4,15 @@ import { BadgeShadeType } from '@/app/_types/interview';
 
 import styles from './Badge.module.scss';
 
-export default function Badge({
-  option: { title, shade = '05' },
-}: {
-  option: { title: string; shade?: BadgeShadeType };
-}) {
-  return <div className={clsx(styles.wrapper, shade && styles[`wrapper-${shade}`])}>{title}</div>;
+interface Props {
+  title: string;
+  shade?: BadgeShadeType;
+}
+
+export default function Badge({ option: { title, shade = '05' } }: { option: Props }) {
+  return <div className={badgeClass(shade)}>{title}</div>;
+}
+
+function badgeClass(type: BadgeShadeType) {
+  return clsx(styles.wrapper, type && styles[`wrapper-${type}`]);
 }
