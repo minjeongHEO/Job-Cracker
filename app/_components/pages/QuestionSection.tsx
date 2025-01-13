689d2e548b4d1f5256e20ec98e95343391b49e7d
minjeongHEO <hmjoung9770@gmail.com>
2025-01-13 12:51:34 +0900

    feat: 로딩 중 일 때 스피너 띄우기 #41

--- app/_components/pages/QuestionSection.tsx
+++ app/_components/pages/QuestionSection.tsx
@@ -6,6 +6,7 @@ import Input from '@/app/_components/molecules/Input';
 import QuestionCard from '@/app/_components/molecules/QuestionCard';
 import { LoadingType, QuestionState } from '@/app/_types/interview';
 
+import SpinnerIcon from '@/app/_components/icons/SpinnerIcon';
 import styles from './QuestionSection.module.scss';
 
 interface QuestionSectionProps {
@@ -26,13 +27,12 @@ export default function QuestionSection({
   handleGenerateFeedbackAnswer,
 }: QuestionSectionProps) {
   return (
-    <div
-      className={clsx(styles['question_section'], { [styles['question_section--with-answer']]: selectedQuestionId })}
-    >
+    <div className={transitionWidthClass(selectedQuestionId)}>
       <header className={styles['question_section__header']}>
         <p className="title">면접 질문에 답변해보세요</p>
         <p className={styles['sub-title']}>꼬리 질문으로 실력 레벨업! 팔로♾️로미 😉</p>
       </header>
+      <SpinnerIcon className={loadingClass(loadingType)} width={80} height={80} />
       <section className={styles['question_section__cards']}>
         {questions.map((question, index) => (
           <QuestionCard
@@ -45,10 +45,20 @@ export default function QuestionSection({
           />
         ))}
       </section>
-
       <div className={styles['question_section__input-container']}>
         <Input handleGenerateFeedbackAnswer={handleGenerateFeedbackAnswer} loadingType={loadingType} />
       </div>
     </div>
   );
+}
+
+function loadingClass(loadingType: LoadingType) {
+  return clsx(styles['loading-spinner'], {
+    [styles['loading-spinner--loading-animation']]: loadingType !== null,
+  });
+}
+function transitionWidthClass(selectedQuestionId: string | undefined) {
+  return clsx(styles['question_section'], {
+    [styles['question_section--with-answer']]: selectedQuestionId,
+  });
 }
