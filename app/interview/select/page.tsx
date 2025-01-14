406b42d7164efdda6797c6a59dd222e280499e5c
minjeongHEO <hmjoung9770@gmail.com>
2025-01-14 14:14:02 +0900

    design: 개발 선택페이지에도 스크롤 적용 #36

--- app/interview/select/page.tsx
+++ app/interview/select/page.tsx
@@ -4,27 +4,29 @@ import Link from 'next/link';
 import { useState } from 'react';
 
 import SelectButton from '@/app/_components/atoms/SelectButton';
+import topicSelectStyles from '@/app/_components/molecules/TopicSelector.module.scss';
+import SelectLayout from '@/app/_components/templates/SelectLayout';
 import { DeveloperType } from '@/app/_types/interview';
 import { typedEntries } from '@/app/_utils/typeUtils';
 import { DEVELOPER_OPTIONS } from '@/app/interview/_constants/developers';
-
-import SelectLayout from '../../_components/templates/SelectLayout';
 
 export default function SelectPage() {
   const [selectedDev, setSelectedDev] = useState<DeveloperType | null>();
 
   return (
     <SelectLayout title={'당신은 어떤 개발자인가요?'} subtitle={'관심 있는 개발 분야를 선택해주세요'}>
-      {typedEntries(DEVELOPER_OPTIONS).map(([key, option]) => (
-        <Link key={key} href={`/interview/select/${key}`}>
-          <SelectButton
-            variant="detailed"
-            option={option}
-            isSelected={selectedDev === key}
-            onClick={() => setSelectedDev(key)}
-          />
-        </Link>
-      ))}
+      <section className={topicSelectStyles['select_container']}>
+        {typedEntries(DEVELOPER_OPTIONS).map(([key, option]) => (
+          <Link key={key} href={`/interview/select/${key}`}>
+            <SelectButton
+              variant="detailed"
+              option={option}
+              isSelected={selectedDev === key}
+              onClick={() => setSelectedDev(key)}
+            />
+          </Link>
+        ))}
+      </section>
     </SelectLayout>
   );
 }
