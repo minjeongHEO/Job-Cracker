7976ce4c1a1801c64a7051ba470e3ccfca0d82eb
minjeongHEO <hmjoung9770@gmail.com>
2025-01-15 14:10:36 +0900

    remove: 에러를 그대로 throw 하던 부분의 try catch 삭제 #37

--- services/api/client.ts
+++ services/api/client.ts
@@ -1,21 +1,17 @@
 /** 공통 fetch 로직 */
 export async function fetchWithErrorHandling(url: string, params: unknown) {
-  try {
-    const response = await fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(params),
-    });
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(params),
+  });
 
-    const errorData = await response.json();
+  const errorData = await response.json();
 
-    if (!response.ok) {
-      throw new Error(errorData.message || 'Server Error: Failed to generate question');
-    }
-    return errorData;
-  } catch (error) {
-    throw error;
+  if (!response.ok) {
+    throw new Error(errorData.message || 'Server Error: Failed to generate question');
   }
+  return errorData;
 }
--- services/interview/service.ts
+++ services/interview/service.ts
@@ -16,12 +16,8 @@ export async function generateQuestion({ devType, subTopics, topics }: GenerateQ
     ', '
   )}에 관련한 면접 질문을 중요도가 높은걸 우선 순위로 하여 랜덤으로 생성해주세요.`;
 
-  try {
-    const parsedResult = await getChatMessage(systemPrompt, userPrompt, isGenerateQuestionResponse);
-    return parsedResult;
-  } catch (error) {
-    throw error;
-  }
+  const parsedResult = await getChatMessage(systemPrompt, userPrompt, isGenerateQuestionResponse);
+  return parsedResult;
 }
 
 /** 다른 질문 생성 */
@@ -38,12 +34,8 @@ export async function generateAnotherQuestion({
   ${topics.join(',')}의 ${subTopics.join(',')}에 대해, 이전 질문과 다른 주제의 면접 질문을 생성해주세요.
   특히 ${questionState.titleTopic}을 제외한 다른 주제에서 중요도가 높은 질문을 우선적으로 생성해주세요.`;
 
-  try {
-    const parsedResult = await getChatMessage(systemPrompt, userPrompt, isGenerateQuestionResponse);
-    return parsedResult;
-  } catch (error) {
-    throw error;
-  }
+  const parsedResult = await getChatMessage(systemPrompt, userPrompt, isGenerateQuestionResponse);
+  return parsedResult;
 }
 
 /** 답변에 대한 피드백 생성 */
@@ -52,10 +44,6 @@ export async function generateFeedbackAnswer({ topics, question, userAnswer }: G
   const userPrompt =
     '답변에 대한 점수 평가, 피드백, 개선된 답변을 제시하고, 답변에서 언급된 개념이나 기술에 대해 더 깊이 있게 확인할 수 있는 꼬리 질문도 함께 생성해주세요.';
 
-  try {
-    const parsedResult = await getChatMessage(systemPrompt, userPrompt, isFeedbackResponse);
-    return parsedResult;
-  } catch (error) {
-    throw error;
-  }
+  const parsedResult = await getChatMessage(systemPrompt, userPrompt, isFeedbackResponse);
+  return parsedResult;
 }
