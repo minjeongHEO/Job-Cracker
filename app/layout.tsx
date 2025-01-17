12ac78253ab3b3780885051c58b596ad74825640
minjeongHEO <hmjoung9770@gmail.com>
2025-01-17 18:14:50 +0900

    feat: 구글폼 연결 버튼 생성 #54

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,11 @@
 import type { Metadata } from 'next';
+import Link from 'next/link';
+
+import PaperAirplaneIcon from '@/app/_components/icons/PaperAirplaneIcon';
+import { GOOGLE_FORM_URL } from '@/app/_constants/form';
+
 import './_styles/globals.scss';
+import styles from './layout.module.scss';
 
 export const metadata: Metadata = {
   title: 'Job Cracker',
@@ -13,7 +19,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="container">{children}</body>
+      <body className="container">
+        <Link href={GOOGLE_FORM_URL} target="_blank" className={styles['google-form']}>
+          <PaperAirplaneIcon />
+        </Link>
+        {children}
+      </body>
     </html>
   );
 }
